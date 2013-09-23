498f4ba42e79e534b598fb15bc4bc1908e7c3335
shday <stephen.h.day@gmail.com>
2013-09-22 20:27:58 -0400

    added updateStates()

--- furnace_logger/furnace_logger.ino
+++ furnace_logger/furnace_logger.ino
@@ -6,35 +6,87 @@
  This example code is in the public domain.
  */
 
-#define Y1pin 6
-#define Y2pin 7
-#define W1pin 8
-#define W2pin 9
-#define Gpin 10
+#define y1Pin 9
+#define y2Pin 8
+#define w1Pin 7
+#define w2Pin 6
+#define gPin 10
+
+boolean y1State = false;
+boolean y2State = false;
+boolean w1State = false;
+boolean w2State = false;
+boolean gState = false;
  
+int updateStates() 
+{
+  boolean y1 = false;
+  boolean y2 = false;
+  boolean w1 = false;
+  boolean w2 = false;
+  boolean g = false;
+  
+  for (int i = 0;i < 5; i++)
+  {
+    y1 = y1 || digitalRead(y1Pin);
+    y2 = y2 || digitalRead(y2Pin);
+    w1 = w1 || digitalRead(w1Pin);
+    w2 = w2 || digitalRead(w2Pin);
+    g = g || digitalRead(gPin);
+    delay(3);
+  }  
+  boolean changed = !((y1==y1State) &&
+                        (y2==y2State) &&
+                        (w1==w1State) &&
+                        (w2==w2State) &&
+                        (g==gState));
+                        
+  y1State = y1;
+  y2State = y2;
+  w1State = w1;
+  w2State = w2;
+  gState = g;
+  return changed; 
+}  
+  
+  
+  
+  
+  
  
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(57600);
-  pinMode(Y1pin,INPUT);
-  pinMode(Y2pin,INPUT);
-  pinMode(W1pin,INPUT);
-  pinMode(W2pin,INPUT);
-  pinMode(Gpin,INPUT);
+  pinMode(y1Pin,INPUT);
+  pinMode(y2Pin,INPUT);
+  pinMode(w1Pin,INPUT);
+  pinMode(w2Pin,INPUT);
+  pinMode(gPin,INPUT);
+  
+  updateStates();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
+  
   uint32_t m = millis();
-  int sensorValue = analogRead(A2);
-  int digitalValue = digitalRead(7);
+  
+  if (updateStates()== true) 
+  {
+  //int sensorValue = analogRead(A2);
   // print out the value you read:
   Serial.print(m);
   Serial.print(", ");
-  Serial.print(digitalValue);
+  Serial.print(gState); 
   Serial.print(", ");
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  Serial.print(y1State);
+  Serial.print(", ");
+    Serial.print(y2State);
+  Serial.print(", ");
+    Serial.print(w1State);
+  Serial.print(", ");
+    Serial.print(w2State);
+  Serial.println("");
+  }
 }
